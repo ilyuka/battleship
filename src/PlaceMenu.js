7433a408a592026ab545ccb077c18ac0c453a2e3
ils01 <theilyashulepov@gmail.com>
2024-01-21 17:25:55 +0300

    can move length >1 ships

--- src/PlaceMenu.js
+++ src/PlaceMenu.js
@@ -28,12 +28,11 @@ export default class PlaceMenu {
             const x = Math.floor(initId / 10);
             const y = initId % 10;
             const shipRef = this.gb.board[x][y];
-            // const length = this.gb.board[x][y].length;
             // console.log(initId, x, y, length);
             // const direction = ...
             if (!this.isMoving) {
                 this.isMoving = true;
-                this.addMouseEnter(initId, x, y, shipRef);
+                this.addMouseEnter(initId, shipRef.x, shipRef.y, shipRef);
             }
         });
     }
@@ -42,20 +41,23 @@ export default class PlaceMenu {
         $("#gbplace")
             .children()
             .map((index, child) => {
-                // $(child).on("mouseenter", (ev) => {
-                //     console.log(ev.target);
-                // });
                 $(child).on("click", (ev) => {
-                    // $(`#${initId}`).removeClass("nothit");
-                    this.gb.board[x][y] = "U";
+                    // upd prev loc
+                    for (let i = y; i < y + shipRef.length; i += 1) {
+                        this.gb.board[x][i] = "U";
+                    }
+                    //upd new loc
                     const newId = ev.target.id;
                     const newX = Math.floor(newId / 10);
                     const newY = newId % 10;
-                    this.gb.board[newX][newY] = shipRef;
+                    for (let i = newY; i < newY + shipRef.length; i += 1) {
+                        this.gb.board[newX][i] = shipRef;
+                    }
+                    shipRef.x = newX;
+                    shipRef.y = newY;
+
                     this.updateBoard();
 
-                    // $(ev.target).addClass("nothit");
-                    // console.log("end", ev.target);
                     this.removeMouseEnter();
                     this.isMoving = false;
                     console.log(this.gb.board);
@@ -89,8 +91,8 @@ export default class PlaceMenu {
     }
 
     fillShips() {
-        this.gb.placeShip(1, 0, 2);
-        // this.gb.placeShip(3, 9, 0);
+        this.gb.placeShip(3, 0, 2);
+        this.gb.placeShip(3, 9, 0);
         // this.gb.placeShip(3, 5, 5);
     }
 }
